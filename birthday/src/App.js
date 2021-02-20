7a6ae0fbe5eed3b111f82b580677f151fe4e3886
Niklas Fischer <nikii17.06@gmail.com>
2021-02-20 21:05:20 +0100

    birthday - Completed Project

--- birthday/src/App.js
+++ birthday/src/App.js
@@ -1,9 +1,24 @@
 import './App.css';
+import React, {useState} from "react";
+import data from "./data";
+import List from "./List";
 
 function App() {
+  const [people, setPeople] = useState(data);
+
+  const clearButton = () => {
+    setPeople([]);
+  }
+
   return (
     <>
-      <p>Hello mr f</p>
+      <div id="card">
+        <h3>{people.length} Birthdays Today</h3>
+        <div className="listContainer">
+          <List people={people} />
+        </div>
+        <button id="btn" onClick={() => clearButton()}>Clear All</button>
+      </div>
     </>
   );
 }
---
+++ birthday/src/List.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import "./List.css";
+
+const List = ({people}) => {
+    return (
+        <React.Fragment>
+            {people.map((person)=> {
+                const {id, name, age, image} = person;
+                return(
+                    <div key={id} className="personContainer">
+                        <img className="personPic" src={image} alt={name}/>
+                        <span className="info">
+                            <span className="personName">{name}</span>
+                            <span className="personAge">{age} years</span>
+                        </span>
+                    </div>
+                );
+            })}
+        </React.Fragment>
+    )
+}
+
+export default List
