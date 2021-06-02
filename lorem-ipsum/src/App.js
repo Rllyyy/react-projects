833592ba93b0f83bbf284e8a2f2683da6b6bfc7b
Niklas Fischer <nikii17.06@gmail.com>
2021-06-02 20:00:52 +0200

    lorem-ipsum - Added option Words

--- lorem-ipsum/src/App.js
+++ lorem-ipsum/src/App.js
@@ -4,10 +4,10 @@ import { TiClipboard } from 'react-icons/ti';
 
 function App() {
   const [count, setCount] = useState(1);
-  const [textString, setTextString] = useState('');
   const [textArray, setTextArray] = useState([]);
   const [selectedType, setSelectedType] = useState('paragraphs');
   const [copyText, setCopyText] = useState('Copy Text');
+  const [showCopyText, setShowCopyText] = useState(false);
 
   //globals
   let dataText = data;
@@ -22,15 +22,22 @@ function App() {
     };
   }, [copyText]);
 
+  /*   useEffect(() => {
+    if (showCopyText) {
+      setShowCopyText(true);
+    } else {
+      setShowCopyText(false);
+    }
+  }, [amount]);
+ */
   //functions
   const randomNumber = () => {
     //Return random number between 0 and 105
     return Math.floor(Math.random() * (dataText.length - 1));
   };
 
-  const handleCopyButtonClick = () => {
-    setCopyText('Saved Text');
-    navigator.clipboard.writeText(textArray.join(' '));
+  const clearOutputLorem = () => {
+    setTextArray([]);
   };
 
   const getParagraphs = (amount, startingDataParagraph) => {
@@ -48,12 +55,13 @@ function App() {
     setTextArray(dataText.slice(startingDataParagraph, amount + startingDataParagraph));
   };
 
+  //Functions get called when user selects "Sentences" and clicks Generate
   const getSentences = (amount, startingDataParagraph) => {
     //The following code is a complete mess but idc. This is just for training react
     let startParagraph = dataText[startingDataParagraph];
     let outputArray = [];
 
-    //split paragraph into one array
+    //split paragraph into one array of sentences
     let paragraphToArray = startParagraph.split('.');
 
     //Remove the last element of the array as it is empty
@@ -95,11 +103,68 @@ function App() {
       }
     }
 
-    //convert Array to outputString
-    let outputString = outputArray.join('.');
+    //Set the UseState value and only add the dot to the output if the sentences is not 0
+    setTextArray([outputArray.length > 0 ? `${outputArray.join('.')}.` : '']);
+  };
 
-    //Only add the dot to the output if the sentences is not 0
-    setTextString(outputString.length > 0 ? `${outputString}.` : '');
+  //Functions get called when user selects "Words" and clicks Generate
+  const getWords = (amount, startingDataParagraph) => {
+    //When I wrote this, only god and I knew how it worked. Now, only god knows!
+    let startParagraph = dataText[startingDataParagraph];
+    let outputArray = [];
+
+    //split paragraph into one array of words
+    let paragraphToArray = startParagraph.split(' ');
+
+    let intNextParagraph = 0;
+    while (paragraphToArray.length < amount) {
+      //check that the next element is not out of array bound
+      if (intNextParagraph + startingDataParagraph < dataText.length - 1) {
+        intNextParagraph++;
+      } else {
+        intNextParagraph = 0;
+        startingDataParagraph = 0;
+      }
+      //get the next paragraph
+      const newParagraph = dataText[startingDataParagraph + intNextParagraph];
+
+      //split new paragraph into one array of words
+      const newParagraphArray = newParagraph.split(' ');
+
+      //Add elements from the array to the old array
+      try {
+        for (let i = 0; i < newParagraphArray.length; i++) {
+          if (paragraphToArray.length < amount) {
+            paragraphToArray.push(newParagraphArray[i]);
+          }
+        }
+      } catch {}
+    }
+
+    for (let i = 0; i < amount; i++) {
+      if (i < paragraphToArray.length) {
+        outputArray.push(paragraphToArray[i]);
+      }
+    }
+
+    //Remove Punctuation
+    const outputArrayPunctuation = outputArray.join(' ');
+    const outputArrayNoPunctuation = outputArrayPunctuation.split('.').join('').split(',').join('');
+
+    //Set the UseState value
+    setTextArray([outputArrayNoPunctuation]);
+  };
+
+  //handle Copy Button Click
+  const handleCopyButtonClick = () => {
+    setCopyText('Saved Text');
+    navigator.clipboard.writeText(textArray.join(' '));
+  };
+
+  //handle change
+  const handleChangeSelect = (e) => {
+    clearOutputLorem();
+    setSelectedType(e.target.value);
   };
 
   //handle Submit
@@ -122,11 +187,19 @@ function App() {
         getSentences(amount, startingDataParagraph);
         break;
       case 'words':
+        getWords(amount, startingDataParagraph);
         break;
       case 'characters':
         break;
       default:
         break;
+    }
+
+    //Only show
+    if (amount > 0) {
+      setShowCopyText(true);
+    } else {
+      setShowCopyText(false);
     }
   };
 
@@ -137,17 +210,17 @@ function App() {
         <h2>Lorem Ipsum Generator</h2>
         <form className='lorem-form' onSubmit={handleSubmit}>
           <input type='number' name='amount' id='amount' value={count} onChange={(e) => setCount(e.target.value)} />
-          <select id='type' name='type' value={selectedType} onChange={(e) => setSelectedType(e.target.value)}>
+          <select id='type' name='type' value={selectedType} onChange={(e) => handleChangeSelect(e)}>
             <option value='paragraphs'>Paragraphs</option>
             <option value='sentences'>Sentences</option>
             <option value='words'>Words</option>
-            <option value='characters'>Characters</option>
           </select>
           <button className='btn' type='submit'>
             Generate
           </button>
         </form>
-        {textArray.length !== 0 ? (
+        {/* Only show Button if there is text displayed */}
+        {showCopyText ? (
           <button className='btn' id='btn-copy' onClick={() => handleCopyButtonClick()}>
             <TiClipboard />
             <span>{copyText}</span>
@@ -155,19 +228,13 @@ function App() {
         ) : (
           ''
         )}
-        {selectedType === 'paragraphs' ? (
-          textArray.map((item, index) => {
-            return (
-              <article className='lorem' key={index}>
-                <p>{item}</p>
-              </article>
-            );
-          })
-        ) : (
-          <article className='lorem'>
-            <p>{textString}</p>
-          </article>
-        )}
+        {textArray.map((item, index) => {
+          return (
+            <article className='lorem' key={index}>
+              <p>{item}</p>
+            </article>
+          );
+        })}
       </main>
     </>
   );
