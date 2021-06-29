356424011156845e0428272e452666400b238061
Niklas Fischer <nikii17.06@gmail.com>
2021-06-29 20:02:51 +0200

    shop - Added more-info hover

--- shop/src/Components/Home.js
+++ shop/src/Components/Home.js
@@ -1,16 +1,22 @@
+import { HiOutlinePlus } from "react-icons/hi";
+
 function Home() {
   return (
     <main className='Home'>
       <h2>Home Content</h2>
       <section className='content'>
         <div className='item'>
+          <div className='product-more-info' onClick={() => console.log("Hello")}>
+            <HiOutlinePlus />
+            <p>Learn More</p>
+          </div>
           <div className='product-image'>
             <img
               src='https://store.storeimages.cdn-apple.com/4668/as-images.apple.com/is/iphone-12-family-select-2021?wid=470&hei=556&fmt=jpeg&qlt=95&.v=1617135051000'
               alt='i-phone'
+              onClick={() => console.log("Hello")}
             />
           </div>
-
           <div className='info'>
             <p className='product-name'>I-Phone</p>
             <p className='product-price'>price</p>
@@ -25,13 +31,17 @@ function Home() {
         <div className='item'></div>
         <div className='item'></div>
         <div className='item'></div>
-
+        <div className='item'></div>
+        <div className='item'></div>
         <div className='item'></div>
         <div className='item'>
+          <div className='product-more-info'>
+            <HiOutlinePlus />
+            <p>Learn More</p>
+          </div>
           <div className='product-image'>
             <img src='https://media.nbb-cdn.de/images/products/640000/646513/samsung_note_9_hero_blau.jpg?size=2800' alt='i-phone' />
           </div>
-
           <div className='info'>
             <p className='product-name'>Galaxy Note 9</p>
             <p className='product-price'>price</p>
